092e193964380aaadf39ebf216a0b54cd6d7fdcb
eoinohall <eohalloran410@gmail.com>
2024-11-08 16:28:18 +0000

    cleaning up

--- week1-usingBokeh.py
+++ week1-usingBokeh.py
@@ -8,21 +8,19 @@ from scipy.signal import find_peaks
 # Graph theme
 curdoc().theme = "dark_minimal"
 
-# Lists used
-xValues = [] # use np array to fill
+# Lists which populate graph
+xValues = []
 yForkValues = []
 yShockValues = []
 
 # Reading length for x values
 with open("exampleTestRun.TXT", "r") as file:
-    lineCount = len(file.readlines())
+    lineCount = len(file.readlines()) - 4 # Skip headers
 
 # Reading raw accelerometer values
 with open("exampleTestRun.TXT", "r") as file:
-    # Skip header (first 2 lines)
-    file.readline()
-    file.readline()
-    for accelerometerInstance in file :
+    file.readline();file.readline() # Skip header
+    for accelerometerInstance in file:
         accelerometerList = accelerometerInstance.split(",")
         if len(accelerometerList)!=1:
             # Axis measured - Update values ##------------------------------------------------------------------------------------------##
@@ -42,7 +40,7 @@ yShockValues = np.array(yShockValues, dtype=float)
 shockPeaks, _ = find_peaks(yShockValues)
 shockTroughs, _ =find_peaks(-yShockValues)
 
-# Create a new plot with a title and axis labels
+# Create a new graph plot with a title and axis labels
 p = figure(
     title="Compression Plot",
     sizing_mode="stretch_width",
@@ -53,22 +51,12 @@ p = figure(
     y_axis_label='Compression (mm)',
     tools="pan, reset, wheel_zoom, xwheel_zoom, fullscreen, examine, crosshair"
 )
-
-# Removing toolbar
 p.toolbar.logo = None
 
 
-
-#---- Data analysis
-print("Shock values:")
-print("Max: ", yShockValues.max(), " Min: ", yShockValues.min(), " Mean: ", yShockValues.mean())
-print("Fork values:")
-print("Max: ", yForkValues.max(), " Min: ", yForkValues.min(), " Mean: ", yForkValues.mean())
-maxYValue = max(yShockValues.max(),yForkValues.max())
-minYValue = min(yShockValues.min(),yForkValues.min())
+#---- Data analysis ---# Move to function or method
+maxYValue = max(yShockValues.max(),yForkValues.max()); minYValue = min(yShockValues.min(),yForkValues.min())
 #----
-
-
 
 # Limit axis movement
 p.x_range = Range1d(start=0, end=timeLength, bounds=(0, timeLength))
@@ -85,6 +73,5 @@ p.scatter(xValues[shockPeaks], yShockValues[shockPeaks], color="red", size=2, le
 p.scatter(xValues[shockTroughs], yShockValues[shockTroughs], color="blue", size=2, legend_label="Shock Troughs", marker="circle")
 
 # Call function to display plot
-p.legend.click_policy = "hide"
-show(p)
+p.legend.click_policy = "hide"; show(p)
 

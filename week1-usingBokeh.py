aa60e33961e2ec4fbe0c714cb4f8c8dd7f00320b
eoinohall <eohalloran410@gmail.com>
2024-11-08 16:08:01 +0000

    cleaning up - trying to include actual values
    Ive adjusted so file is reading all values.
    Time base is going off recorded frequency - so time of recordings is accurate

--- week1-usingBokeh.py
+++ week1-usingBokeh.py
@@ -13,24 +13,27 @@ xValues = [] # use np array to fill
 yForkValues = []
 yShockValues = []
 
-# Reading from file
+# Reading length for x values
+with open("exampleTestRun.TXT", "r") as file:
+    lineCount = len(file.readlines())
+
+# Reading raw accelerometer values
 with open("exampleTestRun.TXT", "r") as file:
     # Skip header (first 2 lines)
     file.readline()
     file.readline()
-    i=0
     for accelerometerInstance in file :
         accelerometerList = accelerometerInstance.split(",")
         if len(accelerometerList)!=1:
-            i+=0.01
-            xValues.append(i) # Adding time base ##------------------------------------------------------------------------------------------##
             # Axis measured - Update values ##------------------------------------------------------------------------------------------##
             yShockValues.append(accelerometerList[0])
             yForkValues.append(accelerometerList[1])
 
-# Convert lists to numpy arrays
-xValues = np.array(xValues, dtype=float)
+# Populating x graph (time axis) into numpy array
+sampleFrequency = 1000; samplePeriod = (1/sampleFrequency); timeLength = lineCount*samplePeriod
+xValues = array = np.arange(0, timeLength, samplePeriod)
 
+# Convert lists to numpy arrays
 yForkValues = np.array(yForkValues, dtype=float)
 forkPeaks, _ = find_peaks(yForkValues)
 forkTroughs, _ =find_peaks(-yForkValues)
@@ -54,26 +57,34 @@ p = figure(
 # Removing toolbar
 p.toolbar.logo = None
 
+
+
+#---- Data analysis
+print("Shock values:")
+print("Max: ", yShockValues.max(), " Min: ", yShockValues.min(), " Mean: ", yShockValues.mean())
+print("Fork values:")
+print("Max: ", yForkValues.max(), " Min: ", yForkValues.min(), " Mean: ", yForkValues.mean())
+maxYValue = max(yShockValues.max(),yForkValues.max())
+minYValue = min(yShockValues.min(),yForkValues.min())
+#----
+
+
+
 # Limit axis movement
-p.x_range = Range1d(start=0, end=5, bounds=(0, 25))
-p.y_range = Range1d(start=-100, end=100, bounds=(-100, 100))
+p.x_range = Range1d(start=0, end=timeLength, bounds=(0, timeLength))
+p.y_range = Range1d(start=minYValue*1.2, end=maxYValue*1.2, bounds=(minYValue*1.2, maxYValue*1.2))
 
 # Rendering Fork line + Points
 p.line(xValues, yForkValues, legend_label="Front Fork", color="grey", line_width=0.5)
-p.circle(xValues[forkPeaks], yForkValues[forkPeaks], color="red", size=2, legend_label="Fork Peaks")
-p.circle(xValues[forkTroughs], yForkValues[forkTroughs], color="blue", size=2, legend_label="Fork Troughs")
+p.scatter(xValues[forkPeaks], yForkValues[forkPeaks], color="red", size=2, legend_label="Fork Peaks", marker="circle")
+p.scatter(xValues[forkTroughs], yForkValues[forkTroughs], color="blue", size=2, legend_label="Fork Troughs", marker="circle")
 
-
+# Rendering Shock line + Points
 p.line(xValues, yShockValues, legend_label="Rear Shock", color="orange", line_width=0.5)
-p.circle(xValues[shockPeaks], yShockValues[shockPeaks], color="red", size=2, legend_label="Shock Peaks")
-p.circle(xValues[shockTroughs], yShockValues[shockTroughs], color="blue", size=2, legend_label="Shock Troughs")
+p.scatter(xValues[shockPeaks], yShockValues[shockPeaks], color="red", size=2, legend_label="Shock Peaks", marker="circle")
+p.scatter(xValues[shockTroughs], yShockValues[shockTroughs], color="blue", size=2, legend_label="Shock Troughs", marker="circle")
 
 # Call function to display plot
 p.legend.click_policy = "hide"
 show(p)
 
-# Print values
-print("Shock values:")
-print("Max: ", yShockValues.max(), " Min: ", yShockValues.min(), " Mean: ", yShockValues.mean())
-print("Fork values:")
-print("Max: ", yForkValues.max(), " Min: ", yForkValues.min(), " Mean: ", yForkValues.mean())

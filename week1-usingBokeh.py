b45228c203bbe9c7cf3cc4ab51c18cfb56fcd888
eoinohall <eohalloran410@gmail.com>
2024-11-12 18:49:58 +0000

    seperating graphs

--- week1-usingBokeh.py
+++ week1-usingBokeh.py
@@ -1,9 +1,9 @@
 # Imports
 from bokeh.io import curdoc
 from bokeh.plotting import figure, show
-import numpy as np
 from bokeh.models import Range1d
 from scipy.signal import find_peaks
+import numpy as np
 
 # Graph theme
 curdoc().theme = "dark_minimal"
@@ -34,14 +34,14 @@ xValues = array = np.arange(0, timeLength, samplePeriod)
 # Convert lists to numpy arrays
 yForkValues = np.array(yForkValues, dtype=float)
 forkPeaks, _ = find_peaks(yForkValues)
-forkTroughs, _ =find_peaks(-yForkValues)
+forkTroughs, _ = find_peaks(-yForkValues)
 
 yShockValues = np.array(yShockValues, dtype=float)
 shockPeaks, _ = find_peaks(yShockValues)
-shockTroughs, _ =find_peaks(-yShockValues)
+shockTroughs, _ = find_peaks(-yShockValues)
 
 # Create a new graph plot with a title and axis labels
-p = figure(
+forkGraph = figure(
     title="Compression Plot",
     sizing_mode="stretch_width",
     height=450,
@@ -51,27 +51,58 @@ p = figure(
     y_axis_label='Compression (mm)',
     tools="pan, reset, wheel_zoom, xwheel_zoom, fullscreen, examine, crosshair"
 )
-p.toolbar.logo = None
+shockGraph = figure(
+    title="Compression Plot",
+    sizing_mode="stretch_width",
+    height=450,
+    x_range=(0, 10),
+    y_range=(-7, 7),
+    x_axis_label='Time (s)',
+    y_axis_label='Compression (mm)',
+    tools="pan, reset, wheel_zoom, xwheel_zoom, fullscreen, examine, crosshair"
+)
+forkGraph.toolbar.logo = None; shockGraph.toolbar.logo = None
 
 
 #---- Data analysis ---# Move to function or method
+# Range of graph in y-axis
 maxYValue = max(yShockValues.max(),yForkValues.max()); minYValue = min(yShockValues.min(),yForkValues.min())
+
+# Calculating gradients
+xForkTurningPoints = [val for pair in zip(forkPeaks, forkTroughs) for val in pair]; xForkTurningPoints.insert(0, 0); xShockTurningPoints = [val for pair in zip(shockPeaks, forkPeaks) for val in pair]
+gradientsFork = []; gradientsShock = []
+for value in xForkTurningPoints:
+    if value == 0: lastValue = value;pass
+    gradientsFork.append((yForkValues[value]-yForkValues[lastValue])/(value-lastValue))
+    lastValue = value
+
+for value in xShockTurningPoints:
+    if value == 0: lastValue = value;pass
+    gradientsShock.append((yShockValues[value]-yShockValues[lastValue])/(value-lastValue))
+    lastValue = value
 #----
 
-# Limit axis movement
-p.x_range = Range1d(start=0, end=timeLength, bounds=(0, timeLength))
-p.y_range = Range1d(start=minYValue*1.2, end=maxYValue*1.2, bounds=(minYValue*1.2, maxYValue*1.2))
 
 # Rendering Fork line + Points
-p.line(xValues, yForkValues, legend_label="Front Fork", color="grey", line_width=0.5)
-p.scatter(xValues[forkPeaks], yForkValues[forkPeaks], color="red", size=2, legend_label="Fork Peaks", marker="circle")
-p.scatter(xValues[forkTroughs], yForkValues[forkTroughs], color="blue", size=2, legend_label="Fork Troughs", marker="circle")
+forkGraph.line(xValues, yForkValues, legend_label="Front Fork", color="grey", line_width=1)
+forkGraph.scatter(xValues[forkPeaks], yForkValues[forkPeaks], color="red", size=2, legend_label="Fork Peaks", marker="circle")
+forkGraph.scatter(xValues[forkTroughs], yForkValues[forkTroughs], color="blue", size=2, legend_label="Fork Troughs", marker="circle")
+
+# Plotting gradients
+forkGraph.line(xValues[xForkTurningPoints], gradientsFork, legend_label="Fork Gradient", color="white", line_width=1)
+
 
 # Rendering Shock line + Points
-p.line(xValues, yShockValues, legend_label="Rear Shock", color="orange", line_width=0.5)
-p.scatter(xValues[shockPeaks], yShockValues[shockPeaks], color="red", size=2, legend_label="Shock Peaks", marker="circle")
-p.scatter(xValues[shockTroughs], yShockValues[shockTroughs], color="blue", size=2, legend_label="Shock Troughs", marker="circle")
+shockGraph.line(xValues, yShockValues, legend_label="Rear Shock", color="orange", line_width=0.5)
+shockGraph.scatter(xValues[shockPeaks], yShockValues[shockPeaks], color="red", size=2, legend_label="Shock Peaks", marker="circle")
+shockGraph.scatter(xValues[shockTroughs], yShockValues[shockTroughs], color="blue", size=2, legend_label="Shock Troughs", marker="circle")
 
-# Call function to display plot
-p.legend.click_policy = "hide"; show(p)
+# Plotting gradients
+shockGraph.line(xValues[xShockTurningPoints], gradientsShock, legend_label="Shock Gradient", color="Brown", line_width=1)
 
+
+# Limit axis movement and display plots
+forkGraph.x_range = Range1d(start=0, end=timeLength, bounds=(0, timeLength)); shockGraph.x_range = Range1d(start=0, end=timeLength, bounds=(0, timeLength))
+forkGraph.y_range = Range1d(start=minYValue*1.2, end=maxYValue*1.2, bounds=(minYValue*1.2, maxYValue*1.2)); shockGraph.y_range = Range1d(start=minYValue*1.2, end=maxYValue*1.2, bounds=(minYValue*1.2, maxYValue*1.2))
+forkGraph.legend.click_policy = "hide"; shockGraph.legend.click_policy = "hide"
+show(forkGraph)

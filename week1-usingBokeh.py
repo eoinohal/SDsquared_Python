1d57055a43f1528e7054638695076499f152cd3d
William Ellis <wellis3@sheffield.ac.uk>
2024-11-13 17:37:16 +0000

    Added useful statistics below the graph, added both testing files into the git and a string to change between them

--- week1-usingBokeh.py
+++ week1-usingBokeh.py
@@ -1,9 +1,13 @@
 # Imports
 from bokeh.io import curdoc
 from bokeh.plotting import figure, show
+from bokeh.models import Range1d, Div
+from bokeh.layouts import Column
 import numpy as np
-from bokeh.models import Range1d
 from scipy.signal import find_peaks
+
+# Defining which text file to user
+textFile = "TestRun2.TXT"
 
 # Graph theme
 curdoc().theme = "dark_minimal"
@@ -14,64 +18,107 @@ yForkValues = []
 yShockValues = []
 
 # Reading length for x values
-with open("exampleTestRun.TXT", "r") as file:
-    lineCount = len(file.readlines()) - 4 # Skip headers
+with open(textFile, "r") as file:
+    lineCount = len(file.readlines()) - 4  # Skip headers
 
 # Reading raw accelerometer values
-with open("exampleTestRun.TXT", "r") as file:
-    file.readline();file.readline() # Skip header
+with open(textFile, "r") as file:
+    file.readline()
+    file.readline()  # Skip header
     for accelerometerInstance in file:
         accelerometerList = accelerometerInstance.split(",")
-        if len(accelerometerList)!=1:
-            # Axis measured - Update values ##------------------------------------------------------------------------------------------##
-            yShockValues.append(accelerometerList[6]) # 6 is the rear shock
-            yForkValues.append((accelerometerList[7])) # 7 is the front fork
+        if len(accelerometerList) != 1:
+            # Axis measured - Update values
+            yShockValues.append(float(accelerometerList[6]) - 5)  # Rear shock
+            yForkValues.append(float(accelerometerList[7]) - 157)  # Front fork
 
 # Populating x graph (time axis) into numpy array
-sampleFrequency = 1000; samplePeriod = (1/sampleFrequency); timeLength = lineCount*samplePeriod
-xValues = array = np.arange(0, timeLength, samplePeriod)
+sampleFrequency = 1000
+samplePeriod = 1 / sampleFrequency
+timeLength = lineCount * samplePeriod
+xValues = np.arange(0, timeLength, samplePeriod)
 
 # Convert lists to numpy arrays
 yForkValues = np.array(yForkValues, dtype=float)
 forkPeaks, _ = find_peaks(yForkValues)
-forkTroughs, _ =find_peaks(-yForkValues)
+forkTroughs, _ = find_peaks(-yForkValues)
 
 yShockValues = np.array(yShockValues, dtype=float)
 shockPeaks, _ = find_peaks(yShockValues)
-shockTroughs, _ =find_peaks(-yShockValues)
+shockTroughs, _ = find_peaks(-yShockValues)
 
 # Create a new graph plot with a title and axis labels
 p = figure(
     title="Compression Plot",
     sizing_mode="stretch_width",
     height=450,
-    x_range=(0, 10),
-    y_range=(-7, 7),
-    x_axis_label='Time (s)',
-    y_axis_label='Compression (mm)',
-    tools="pan, reset, wheel_zoom, xwheel_zoom, fullscreen, examine, crosshair"
+    x_axis_label="Time (s)",
+    y_axis_label="Compression (mm)",
+    tools="pan, reset, wheel_zoom, xwheel_zoom, fullscreen, examine, crosshair",
 )
 p.toolbar.logo = None
 
-
-#---- Data analysis ---# Move to function or method
-maxYValue = max(yShockValues.max(),yForkValues.max()); minYValue = min(yShockValues.min(),yForkValues.min())
-#----
+# Data analysis
+shockMax, shockMin, shockMean = yShockValues.max(), yShockValues.min(), yShockValues.mean()
+forkMax, forkMin, forkMean = yForkValues.max(), yForkValues.min(), yForkValues.mean()
 
 # Limit axis movement
 p.x_range = Range1d(start=0, end=timeLength, bounds=(0, timeLength))
-p.y_range = Range1d(start=minYValue*1.2, end=maxYValue*1.2, bounds=(minYValue*1.2, maxYValue*1.2))
+p.y_range = Range1d(start=min(yShockValues.min(), yForkValues.min()) * 1.2,
+                    end=max(yShockValues.max(), yForkValues.max()) * 1.2)
 
 # Rendering Fork line + Points
-p.line(xValues, yForkValues, legend_label="Front Fork", color="grey", line_width=0.5)
-p.scatter(xValues[forkPeaks], yForkValues[forkPeaks], color="red", size=2, legend_label="Fork Peaks", marker="circle")
-p.scatter(xValues[forkTroughs], yForkValues[forkTroughs], color="blue", size=2, legend_label="Fork Troughs", marker="circle")
+p.line(xValues, yForkValues, legend_label="Front Fork", color="#00FFFF", line_width=0.5)
+p.scatter(
+    xValues[forkPeaks],
+    yForkValues[forkPeaks],
+    color="red",
+    size=2,
+    legend_label="Fork Peaks",
+    marker="circle",
+)
+p.scatter(
+    xValues[forkTroughs],
+    yForkValues[forkTroughs],
+    color="orange",
+    size=2,
+    legend_label="Fork Troughs",
+    marker="circle",
+)
 
 # Rendering Shock line + Points
-p.line(xValues, yShockValues, legend_label="Rear Shock", color="orange", line_width=0.5)
-p.scatter(xValues[shockPeaks], yShockValues[shockPeaks], color="red", size=2, legend_label="Shock Peaks", marker="circle")
-p.scatter(xValues[shockTroughs], yShockValues[shockTroughs], color="blue", size=2, legend_label="Shock Troughs", marker="circle")
+p.line(xValues, yShockValues, legend_label="Rear Shock", color="#FF9500", line_width=0.5)
+p.scatter(
+    xValues[shockPeaks],
+    yShockValues[shockPeaks],
+    color="red",
+    size=2,
+    legend_label="Shock Peaks",
+    marker="circle",
+)
+p.scatter(
+    xValues[shockTroughs],
+    yShockValues[shockTroughs],
+    color="orange",
+    size=2,
+    legend_label="Shock Troughs",
+    marker="circle",
+)
 
-# Call function to display plot
-p.legend.click_policy = "hide"; show(p)
+# Display stats as HTML below the plot
+stats_div = Div(
+    text=f"""
+    <h3>Shock Values:</h3>
+    <p><b>Max:</b> {shockMax:.2f} mm<br><b>Min:</b> {shockMin:.2f} mm<br><b>Mean:</b> {shockMean:.2f} mm</p>
+    <h3>Fork Values:</h3>
+    <p><b>Max:</b> {forkMax:.2f} mm<br><b>Min:</b> {forkMin:.2f} mm<br><b>Mean:</b> {forkMean:.2f} mm</p>
+    """,
+    sizing_mode="stretch_width",
+)
 
+p.legend.click_policy = "hide"
+# Combine plot and stats into a layout
+layout = Column(p, stats_div, sizing_mode="stretch_both")
+
+# Show the plot with stats
+show(layout)

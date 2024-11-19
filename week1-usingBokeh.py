ae387eff3fc297f93cedf28650a4e08fa771007e
eoinohall <eohalloran410@gmail.com>
2024-11-19 18:03:03 +0000

    Percentage displacement

--- week1-usingBokeh.py
+++ week1-usingBokeh.py
@@ -6,37 +6,42 @@ from bokeh.layouts import Column
 import numpy as np
 from scipy.signal import find_peaks
 
-# Defining which text file to user
+# Text file data is retreived from
 textFile = "TestRun1.TXT"
 
 # Graph theme
 curdoc().theme = "dark_minimal"
 
+# Travel constants
+FORK_TRAVEL = 170; SHOCK_TRAVEL = 170; BIT_RANGE = 1024 #Bitrange is range of measured values
+
 # Lists which populate graph
-xValues = []
-yForkValues = []
-yShockValues = []
+xValues = []; yForkValues = []; yShockValues = []
 
-# Reading length for x values
-with open(textFile, "r") as file:
-    lineCount = len(file.readlines()) - 4  # Skip headers
+# Defining number of entries
+with open(textFile, "r") as file:lineCount = len(file.readlines()) - 4  # skipping header and footer
 
-# Reading raw accelerometer values
+# Reading raw accelerometer values from file
 with open(textFile, "r") as file:
-    file.readline()
-    file.readline()  # Skip header
+    file.readline()  # Header in format {shorthand name: sampling frequency: longhand name}
+    initialValues = file.readline().split(',') # Initial values in list [shock displacement, fork displacement,braking, braking]
+    initialShockDisplacement = float(initialValues[0]); initialForkDisplacement = float(initialValues[1])
     for accelerometerInstance in file:
         accelerometerList = accelerometerInstance.split(",")
         if len(accelerometerList) != 1:
-            # Axis measured - Update values
-            yShockValues.append(float(accelerometerList[6]) - 5)  # Rear shock
-            yForkValues.append(float(accelerometerList[7]) - 157)  # Front fork
+            # Calculating percentage from Bit range
+            yShockValues.append(((float(accelerometerList[6])-initialShockDisplacement)/BIT_RANGE)*100)  # Rear shock
+            yForkValues.append(((float(accelerometerList[7])-initialForkDisplacement)/BIT_RANGE)*100)  # Front fork
+        else:
+            if accelerometerInstance!='Run finished\n': # Footer with time of run value
+                timeOfRun = int(accelerometerInstance)/1000
+
+# Time values
+sampleFrequency = lineCount/timeOfRun
+samplePeriod = 1/sampleFrequency
 
 # Populating x graph (time axis) into numpy array
-sampleFrequency = 1000
-samplePeriod = 1 / sampleFrequency
-timeLength = lineCount * samplePeriod
-xValues = np.arange(0, timeLength, samplePeriod)
+xValues = np.arange(0, timeOfRun, samplePeriod)
 
 # Convert lists to numpy arrays
 yForkValues = np.array(yForkValues, dtype=float)
@@ -48,7 +53,7 @@ forkTroughTimes = xValues[forkTroughs]
 
 # Initialize combined lists
 time_differences = []
-compression_differences = []
+displacement_differences = []
 
 # Loop through each peak
 for peak, peak_time in zip(forkPeaks, forkPeakTimes):
@@ -63,36 +68,35 @@ for peak, peak_time in zip(forkPeaks, forkPeakTimes):
         time_diff = following_trough - peak_time
         time_differences.append(time_diff)
 
-        # Calculate the compression difference
+        # Calculate the displacement difference
         trough_value = yForkValues[forkTroughs[np.where(forkTroughTimes == following_trough)[0][0]]]
-        compression_diff = yForkValues[peak] - trough_value
-        compression_differences.append((peak_time, compression_diff, time_diff))
+        displacement_diff = yForkValues[peak] - trough_value
+        displacement_differences.append((peak_time, displacement_diff, time_diff))
     else:
         # Handle case where there is no following trough
         time_differences.append(np.nan)  # NaN for no following trough
-        compression_differences.append((peak_time, np.nan, np.nan))  # NaN for no following trough
+        displacement_differences.append((peak_time, np.nan, np.nan))  # NaN for no following trough
 
 # Convert results to formatted strings
 formatted_differences = [
-    f"{time:.3f}s: {time_diff:.3f}s: {compression_diff:.2f} mm: {compression_diff / time_diff:} mm/s"
-    if not (np.isnan(compression_diff) or np.isnan(time_diff))
+    f"{time:.3f}: {time_diff:.3f}: {displacement_diff:.2f}: {displacement_diff / time_diff:}"
+    if not (np.isnan(displacement_diff) or np.isnan(time_diff))
     else f"{time:.3f}s: No trough"
-    for time, compression_diff, time_diff in compression_differences
+    for time, displacement_diff, time_diff in displacement_differences
 ]
 
-
-
+# Calculating turning points
 yShockValues = np.array(yShockValues, dtype=float)
 shockPeaks, _ = find_peaks(yShockValues)
 shockTroughs, _ = find_peaks(-yShockValues)
 
-# Create a new graph plot with a title and axis labels
+##--- Displacement plot graph ---##
 p = figure(
-    title="Compression Plot: " + textFile,
+    title="Displacement Plot: " + textFile,
     sizing_mode="stretch_width",
     height=450,
     x_axis_label="Time (s)",
-    y_axis_label="Compression (mm)",
+    y_axis_label="Percentage displacement (%)",
     tools="pan, reset, wheel_zoom, xwheel_zoom, fullscreen, examine, crosshair",
 )
 p.toolbar.logo = None
@@ -102,9 +106,8 @@ shockMax, shockMin, shockMean = yShockValues.max(), yShockValues.min(), yShockVa
 forkMax, forkMin, forkMean = yForkValues.max(), yForkValues.min(), yForkValues.mean()
 
 # Limit axis movement
-p.x_range = Range1d(start=0, end=timeLength, bounds=(0, timeLength))
-p.y_range = Range1d(start=min(yShockValues.min(), yForkValues.min()) * 1.2,
-                    end=max(yShockValues.max(), yForkValues.max()) * 1.2)
+p.x_range = Range1d(start=0, end=timeOfRun, bounds=(0, timeOfRun)) # Display whole time range
+p.y_range = Range1d(start=0, end=100, bounds=(0, 100))
 
 # Rendering Fork line + Points
 p.line(xValues, yForkValues, legend_label="Front Fork", color="#00FFFF", line_width=0.5)
@@ -151,7 +154,7 @@ stats_div = Div(
     <p><b>Max:</b> {shockMax:.2f} mm<br><b>Min:</b> {shockMin:.2f} mm<br><b>Mean:</b> {shockMean:.2f} mm</p>
     <h3>Fork Values:</h3>
     <p><b>Max:</b> {forkMax:.2f} mm<br><b>Min:</b> {forkMin:.2f} mm<br><b>Mean:</b> {forkMean:.2f} mm</p>
-    <h3>Fork Compression Differences:</h3>
+    <h3>Fork Displacement Differences:</h3>
     <ul>
     {''.join(f'<li>{item}</li>' for item in formatted_differences)}
     </ul>
@@ -160,8 +163,47 @@ stats_div = Div(
 )
 
 p.legend.click_policy = "hide"
+
 # Combine plot and stats into a layout
 layout = Column(p, stats_div, sizing_mode="stretch_both")
 
 # Show the plot with stats
 show(layout)
+
+'''
+##--- Scatter Plot ---##
+# Create a new graph plot with a title and axis labels
+p = figure(
+    title="XXX scatter plot", # Give graph suitable name
+    sizing_mode="stretch_width",
+    height=500,
+    x_axis_label="Absolute displacement length (mm)",
+    y_axis_label="Compression speed (mm/s)",
+    tools="pan, reset, wheel_zoom, xwheel_zoom, fullscreen, examine, crosshair",
+)
+
+displacementTimeList = []
+displacementSpeedList = []
+{''.join(f'<li>{item}</li>' for item in formatted_differences)}
+for item in formatted_differences:
+    split = item.split(': ')
+    if split[-1]!='No trough': #skipping over empty cases
+        print(split)
+        displacementTimeList.append(split[1])
+        displacementSpeedList.append(split[3])
+
+print(displacementTimeList)
+print(displacementSpeedList)
+p.scatter(
+    displacementTimeList,
+    displacementSpeedList,
+    color="red",
+    size=2,
+    legend_label="Fork Peaks",
+    marker="circle",
+)
+
+
+# show the results
+show(p)
+'''

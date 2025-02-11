3abcd631c58033b789c0edbad05451ac3a09fe31
William Ellis <wellis3@sheffield.ac.uk>
2025-02-11 17:12:10 +0000

    more sensible text data being outputted for microPythonRunner.py

--- microPythonRunner.py
+++ microPythonRunner.py
@@ -1,13 +1,8 @@
-from dashboard import load_and_process_data
+from dashboard import load_and_process_data, create_stats_div
+
 
 def print_data():
     text_file = "run_data/testrun1.txt"
     data = load_and_process_data(text_file)
-
-    selected_keys = ["timeOfRun","forkCompression_regress", "forkRebound_regress", "shockCompression_regress", "shockRebound_regress"]
-
-    if data:
-        for key in selected_keys:
-            print(f"{key}: {data[key]}")
-
+    print(data["textData"])
 print_data()
